b91fb8357cc8ab591c16c8f7b9043f6e088f31aa
Volkmar Lecke <v.lecke@hsn-agentur.com>
2024-02-05 12:36:43 +0100

    cleanup

--- assets/theme.js
+++ assets/theme.js
@@ -3832,7 +3832,6 @@ $('#quantity-hidden').val($('#customQuantitySelector').find('li[aria-selected="t
   };
 
   theme.addedToCartHandler = function (response) {
-    console.log('xyzabc');
     if (theme.settings.after_add_to_cart === 'page') {
       // redirect
       window.location = theme.routes.cart_url;
@@ -7506,9 +7505,8 @@ $('#quantity-hidden').val($('#customQuantitySelector').find('li[aria-selected="t
       var adj = $(this).data('quantity') == 'up' ? 1 : -1;
       var $qty = $(this).closest('.quantity-wrapper').find('[name=quantity]');
       if ($qty.attr('step')) {
-      // VLE  adj *= parseInt($qty.attr('step'));
+        adj *= parseInt($qty.attr('step'));
       }
-      console.log(adj);
       $qty.val(Math.max(1, parseInt($qty.val()) + adj)).trigger('change');
       return false;
     });
